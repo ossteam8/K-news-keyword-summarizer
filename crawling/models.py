67e30b83294099e077d5717ec3d0663aaa9d3597
jjaen0823 <poungki0823@gmail.com>
2021-05-11 18:42:10 +0900

    modified model

--- crawling/models.py
+++ crawling/models.py
@@ -1,25 +1,34 @@
 from django.db import models
 from picklefield.fields import PickledObjectField
-# from jsonfield import JSONField
+
 #   memo = JSONField(default={}, dump_kwargs={'ensure_ascii': False})s
 
 
 # Create your models here.
+class Category(models.Model):
+    caterory = models.CharField(max_length=64)
+    keywords = models.JSONField(default=dict)
+    
 
-# class Article(models.Model):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-#     search_word = models.ForeignKey(SearchWord, on_delete=models.CASCADE, null=True, blank=True)
+class SearchWord(models.Model):
+    search_word = models.CharField(max_length=64)
+    keywords = models.JSONField(default=dict)
 
-#     title = models.CharField(max_length=128)
-#     contents = models.TextField()
-#     url = models.URLField()
 
-#     summary = models.TextField()
-    # vectors = PickledObjectField()
-#     similarity = models.FloatField(localize=False)  # localize=False 일 때 NumberInput, 그 외 TextInput
+class Article(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    search_word = models.ForeignKey(SearchWord, on_delete=models.CASCADE, null=True, blank=True)
 
-    # def __str__(self):
-    #         return self.title
+    title = models.CharField(max_length=128)
+    contents = models.TextField()
+    url = models.URLField()
+
+    summary = models.TextField()
+    vectors = PickledObjectField()
+    similarity = models.FloatField()  # localize=False 일 때 NumberInput, 그 외 TextInput
+
+    def __str__(self):
+            return self.title
 
     
     # pickle 예시
@@ -40,9 +49,3 @@ from picklefield.fields import PickledObjectField
     # category = models.CharField(max_length=64, choices=CATEGORY_CHOICES)
 
 
-# class Category(models.Model):
-#     caterory = models.CharField(max_length=64)
-
-
-# class SearchWord(models.Model):
-#     search_word = models.CharField(max_length=64)
--- keywords/models.py
+++ keywords/models.py
@@ -3,18 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-# class Keyword(models.Model):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-#     search_word = models.ForeignKey(SearchWord, on_delete=models.CASCADE, null=True, blank=True)
-#     keyword = models.CharField(max_length=64)
 
-    
-#     CATEGORY_CHOICES = [
-#         ('정치', 'politic'),
-#         """"""
-#     ]
-#     category = models.CharField(max_length=64, choices=CATEGORY_CHOICES)
-#     label = models.CharField(max_length=64)
 
 
 
