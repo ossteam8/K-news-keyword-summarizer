2b6072c5240a118138e8f85e172a6e1cba4da784
jjaen0823 <poungki0823@gmail.com>
2021-05-19 23:53:11 +0900

    modified model, view

--- crawling/models.py
+++ crawling/models.py
@@ -7,26 +7,33 @@ from picklefield.fields import PickledObjectField
 # Create your models here.
 class Category(models.Model):
     caterory = models.CharField(max_length=64)
-    keywords = models.JSONField(default=dict)
+    keywords = models.JSONField(default=dict, null=True, blank=True)
+
+    def __str__(self):
+            return self.caterory
+
 # politic, economic, social
 
 class SearchWord(models.Model):
     search_word = models.CharField(max_length=64)
-    keywords = models.JSONField(default=dict)
+    keywords = models.JSONField(default=dict, null=True, blank=True)
+
+    def __str__(self):
+            return self.search_word
 
 
 class Article(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     search_word = models.ForeignKey(SearchWord, on_delete=models.CASCADE, null=True, blank=True)
 
     title = models.CharField(max_length=128)
     contents = models.TextField()
     url = models.URLField()
-
-    summary = models.TextField()
-    vectors = PickledObjectField()
-    similarity = models.FloatField()  # localize=False 일 때 NumberInput, 그 외 TextInput
     register_date = models.DateTimeField(auto_now_add=True)
+
+    summary = models.TextField(null=True, blank=True)
+    vectors = PickledObjectField(null=True, blank=True)
+    similarity = models.FloatField(null=True, blank=True)  # localize=False 일 때 NumberInput, 그 외 TextInput
 
     def __str__(self):
             return self.title
@@ -34,8 +41,8 @@ class Article(models.Model):
     
     # pickle 예시
     # from picklefield.fields import PickledObjectField
-    #     class SomeObject(models.Model):
-    #         args = PickledObjectField()
+    # class SomeObject(models.Model):
+    #     args = PickledObjectField()
 
     #     obj = SomeObject()
     #     obj.args = ['fancy', {'objects': 'inside'}]
--- crawling/views.py
+++ crawling/views.py
@@ -6,7 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView  # show data
 from django.views.generic.edit import CreateView, UpdateView, DeleteView  # add data
 
-from .models import Article
+from .models import Article, Category
 
 
 # Create your views here.
@@ -17,16 +17,16 @@ def keywordCharts(request):
     return render(request, 'crawling/keywordCharts.html')
 
 
-# class ProductList(ListView):
-#     model = Product
-#     template_name = 'product/product.html'
-#     # variable name to use in template(html)
-#     context_object_name = 'product_list'
+class CategoryKeywordsListView(ListView):
+    template_name = 'crawlint/keywordCharts.html'
+    
+    context_object_name = 'keywords'  # view에서 template 에 전달할 context 변수명을 지정함
 
-
-# class KeywordListView(generic.ListView):
-#     template_name = 'dsfdsfdsf.html'
-#     models = Keyword
+    def get_queryset(self, **kwargs):
+        queryset = Category.objects.filter(
+            category=self.request.session.get('category') 
+        )
+        return queryset.keywords  # 해당 카테고리의 키워드 반환
 
 
 class ArticleListView(ListView):
@@ -46,3 +46,15 @@ class ArticleDetailView(DetailView):
 # filtering content like query
 # """
 
+
+
+
+# def date_view(self, request):
+#         week_date = datetime.datetime.now() - datetime.timedelta(days=7)
+#         week_data = Order.objects.filter(register_date__gte=week_date)
+#         data = Order.objects.filter(register_date__lt=week_date)
+#         context = dict(
+#             self.admin_site.each_context(request),
+#             week_data=week_data,
+#             data=data,
+#         )
