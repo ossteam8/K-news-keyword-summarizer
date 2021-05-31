ea59cdaed13bab18843c3f931a4091929ab0d8ce
jjaen0823 <poungki0823@gmail.com>
2021-06-01 01:58:00 +0900

    keywords page

--- crawling/views.py
+++ crawling/views.py
@@ -1,3 +1,5 @@
+import json
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # from django.urls import reverse_lazy
@@ -14,42 +16,50 @@ from .models import Article, Category
 
 # Create your views here.
 def index(request):
-    return render(request, 'crawling/index.html')
+    category_list = Category.objects.all()
+    return render(request, 'crawling/index.html', {'category_list': category_list})
 
-def keywords(request):
-    # 기사 키워드 render
-    return render(request, 'crawling/keywords.html')
+# def keywords(request):
+#     # 기사 키워드 render
+#     return render(request, 'crawling/keywords.html')
 
 
 # category 선택 시, 해당 category 의 keywords 를 보여줌!
-class CategoryKeywordsListView(ListView):
+class CategoryDetailView(DetailView):
     template_name = 'crawling/keywords.html' 
-    context_object_name = 'keywords_list'  # view에서 template 에 전달할 context 변수명을 지정함
+    # context_object_name = 'keywords_list'  # view에서 template 에 전달할 context 변수명을 지정함
 
-    def get_queryset(self):
-        category = self.request.GET.get('category')
-        queryset = Category.objects.filter(category=category).values('keywords')[0] # {{'k1': w1, 'k2': w2, 'k3': w3, ...}}
-        # keywords_dict[0]: {'k1': w1, 'k2': w2, 'k3': w3, ...}
-        # 출력 
-        print(category)
-        print(queryset)
+    def get(self, request, category_id):
+        print("category_id",category_id)
+        # queryset: dict {'k1': w1, 'k2': w2, 'k3': w3, ...}
+        queryset = Category.objects.filter(id=category_id).values('keywords')[0]['keywords']# {{'k1': w1, 'k2': w2, 'k3': w3, ...}}
+        # querysetJson = json.dumps(queryset)
+        articles = Article.objects.prefetch_related('category').filter(category_id=category_id).order_by('register_date')
+        # == articles = Article.objects.select_related('category').filter(category_id=category_id)
 
-        return queryset
-
-# get_context_data: 다른 객체들도 context에 넣어보내고 싶을 때 사용하는 method
-# context = super(Category, self).get_context_data(**kwargs)
+        # print(querysetJson)
+        # print(type(querysetJson))
+        # print(queryset)
+        # print(type(queryset))
+        print(articles)
+        
+        return render(request, self.template_name, {'keywords_list': queryset, 'articles_list': articles})
 
 
 # 키워드 선택 -> 기사 나열!!  
 # prefetch_related: 역방향 참조 이용해서 해당 카테고리에 있는 article을 가져와야함.
-# category = Category.objects.prefetch_related(Prefetch('article_set', queryset=Post.objects.all()))
-# # 기사 가져올 때 해당 키워드가 있어야하는데 
-# class ArticleListView(ListView):
-#     # model 속성으로 객체를 지정하는 것은 모든 generic view에서 가능.
-#     category = Category.objects.prefetch_related(Prefetch('article_set', queryset=Category.objects.all()))
+# category = Category.objects.prefetch_related(Prefetch('article_set'))
+# # 기사 가져올 때 해당 키워드가 있어야하는데 어떻게 구현??
+# class ArticleDetailView(DetailView):
+#     #
+#     category = Category.objects.prefetch_related(Prefetch('article_set')).filter(id=category_id)
+#     print(category)
+        # for c in category:
+        #     print(c.category)  # 정치
+#     template_name = 'crawling/articles.html'
+#     context_object_name = 'articles_list'
 
-#     template_name = 'crawling/index.html'
-#     context_object_name = 'article_list'
+
 
 
 # summary 화면
